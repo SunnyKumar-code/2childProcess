eee804fbbf5eb01fc19efdcb31f06c282df51546
Sunny Kumar <sk202542@gmail.com>
2025-02-16 19:30:37 +0530

    add

---
+++ index.js
@@ -0,0 +1,7 @@
+/**
+ * 1 child process
+ */
+
+const childProcess=require("node:child_process")
+// childProcess.exec("start calc")
+childProcess.exec("start www.google.com")
---
+++ welcome_package/index.js
@@ -0,0 +1,4 @@
+const welcomeUser=(username="")=>{
+    console.log(`Welcomw, ${username}`);
+}
+module.exports = { welcomeUser}
